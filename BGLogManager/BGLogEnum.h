21b4afe301704115385073ed4506916123962bd4
Jeonbyeunggirl <sodtms@gmail.com>
2019-07-08 16:04:59 +0900

    [1.0.0] lib 작업 : BGPrototype 프로젝트에 있는 BGLogManager 기능 라이브러리화 작업

---
+++ BGLogManager/BGLogEnum.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+enum class ELogLevel
+{
+	BG_NONE,
+	BG_DEBUG,
+	BG_INFO,
+	BG_WARNING,
+	BG_ERROR,
+	BG_FATAL,
+
+	BG_LOG_SYSTEM_EXIT
+};
--- BGLogManager/stdafx.h
+++ BGLogManager/stdafx.h
@@ -12,3 +12,15 @@
 
 
 // TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
+#include <windows.h>
+
+#include <thread>
+#include <mutex>
+
+#include <fstream>
+#include <string>
+
+#include <queue>
+#include <map>
+#include <vector>
+
