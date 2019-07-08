30509103a23972a13b85b5ab1987bacb3ab47b02
Jeonbyeunggirl <sodtms@gmail.com>
2019-07-08 17:18:10 +0900

    솔루션, 프로젝트 정리
    프로젝트 삭제했다가 지워서 정리 제대로 안되었다..

--- BGLibraryTestProject/BGLibraryTestProject.cpp
+++ BGLibraryTestProject/BGLibraryTestProject.cpp
@@ -3,7 +3,6 @@
 
 #include "stdafx.h"
 
-#include "BG_LIBRARY\Include\BGLogManager.h"
 
 
 int main()
@@ -11,8 +10,10 @@ int main()
 	g_LogManager.Start();
 
 	BG_LOG_DEBUG("test");
-	
-	Sleep(100000);
+
+
+
+
 
 	g_LogManager.Stop();
 
